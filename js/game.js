be8839f1dd50685ce04ab790af521513a1f0b7f4
feldhaus <maicon.feldhaus@gmail.com>
2017-12-09 19:04:08 -0200

    Added player sprite animations

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 var game;
 
 // main game options
-var gameOptions = {
-    gameWidth: 288,
-    gameHeight: 192
+const gameOptions = {
+    gameWidth: 300,
+    gameHeight: 216
 }
 
 // when the window finishes loading...
@@ -19,13 +19,19 @@ TheGame.prototype = {
 
     // preloading assets
     preload: function () {
-        game.load.image('player', 'assets/images/player.png');
         // environment
-        game.load.image('background', 'assets/images/background.png');
-        game.load.image('middleground', 'assets/images/middleground.png');
+        game.load.image('background', 'assets/images/plx-1.png');
+        game.load.image('middleground1', 'assets/images/plx-2.png');
+        game.load.image('middleground2', 'assets/images/plx-3.png');
+        game.load.image('middleground3', 'assets/images/plx-4.png');
+        game.load.image('middleground4', 'assets/images/plx-5.png');
+        
         // tileset
         game.load.image('tileset', 'assets/images/tileset.png');
         game.load.tilemap('map', 'assets/maps/map.json', null, Phaser.Tilemap.TILED_JSON);
+        
+        // player
+        game.load.spritesheet('player', 'assets/images/player.png', 23, 42);
     },
 
     // when the game starts
@@ -34,8 +40,6 @@ TheGame.prototype = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
-        
-        //tileset
         
         this.createBackgrounds();
         this.createWorld();
@@ -47,9 +51,15 @@ TheGame.prototype = {
     
     createBackgrounds: function () {
         this.background = game.add.tileSprite(0, 0, gameOptions.gameWidth, gameOptions.gameHeight, 'background');
-        this.middleground = game.add.tileSprite(0, 80, gameOptions.gameWidth, gameOptions.gameHeight, 'middleground');
         this.background.fixedToCamera = true;
-        this.middleground.fixedToCamera = true;
+        this.middleground1 = game.add.tileSprite(0, 0, gameOptions.gameWidth, gameOptions.gameHeight, 'middleground1');
+        this.middleground1.fixedToCamera = true;
+        this.middleground2 = game.add.tileSprite(0, 0, gameOptions.gameWidth, gameOptions.gameHeight, 'middleground2');
+        this.middleground2.fixedToCamera = true;
+        this.middleground3 = game.add.tileSprite(0, 0, gameOptions.gameWidth, gameOptions.gameHeight, 'middleground3');
+        this.middleground3.fixedToCamera = true;
+        this.middleground4 = game.add.tileSprite(0, 0, gameOptions.gameWidth, gameOptions.gameHeight, 'middleground4');
+        this.middleground4.fixedToCamera = true;
     },
     
     createWorld: function () {
@@ -85,35 +95,23 @@ TheGame.prototype = {
                 }
             }
         }
-        
-        /*
-        var grab = game.add.graphics(0, 0);
-        grab.beginFill(0xff00ff);
-        grab.drawRect(0, 0, 16, 16);
-        grab.endFill();
-        grab.position.set(16*10, 16*11);
-        */
     },
     
     createPlayer: function () {
         this.player = game.add.sprite(0, 0, 'player');
-        this.player.anchor.setTo(0.5);
+        this.player.animations.add('idle', [0,1,2,3,4,5,6,7,8,9,10,11]);
+        this.player.animations.add('jump', [12]);
+        this.player.animations.add('landing', [13]);
+        this.player.animations.add('ledge_grab', [14,15,16,17,18,19]);
+        this.player.animations.add('fall', [20,21]);
+        this.player.animations.add('run', [22,23,24,25,26,27,28,29]);
+        this.player.animations.play('idle', 12, true);
+        
+        this.player.anchor.set(0.5, 1);
         this.player.position.set(26, 50);
         game.physics.arcade.enable(this.player);
         this.player.body.gravity.y = 500;
-        this.player.body.setSize(20,30,0,0);
-        
-        /*
-        this.player = game.add.graphics(0, 0);
-        this.player.beginFill(0xff3300);
-        this.player.drawRect(-8, -8, 16, 16);
-        this.player.endFill();
-        this.player.position.set(16+8, 16+8);
-        //this.player.anchor.setTo(0.5);
-        game.physics.arcade.enable(this.player);
-        this.player.body.gravity.y = 500;
-        this.player.body.setSize(16, 16, -8, -8);
-        */
+        this.player.body.setSize(12, 28, 6, 6);
     },
     
     bindKeys: function () {
@@ -142,6 +140,7 @@ TheGame.prototype = {
     },
     
     movePlayer: function () {
+        // jump
         if (this.input.jump.isDown && this.player.body.onFloor()) {
             this.player.body.velocity.y = -200;
         }
@@ -154,11 +153,11 @@ TheGame.prototype = {
         var vel = 150;
         if (this.input.left.isDown) {
             this.player.body.velocity.x = -vel;
-            //this.player.animations.play('run');
+            this.player.animations.play('run', 12);
             this.player.scale.x = -1;
         } else if (this.input.right.isDown) {
             this.player.body.velocity.x = vel;
-            //this.player.animations.play('run');
+            this.player.animations.play('run', 12);
             this.player.scale.x = 1;
         } else if (this.input.crouch.isDown) {
             this.player.body.enable = true;
@@ -167,55 +166,44 @@ TheGame.prototype = {
             if (this.input.crouch.isDown) {
             //    this.player.animations.play('crouch');
             } else {
-            //    this.player.animations.play('idle');
+                this.player.animations.play('idle', 12);
             }
         }
         
-        //game.debug.text('speed: '+this.player.body.velocity.y, 10, 10);
-        game.debug.text('px: ' + this.player.x, 10, 10);
+        // jump animation
         if (this.player.body.velocity.y < 0) {
-            return;
+            this.player.animations.play('jump');
+        } else if (this.player.body.velocity.y > 0) {
+            this.player.animations.play('fall');
         }
         
-        var px = this.player.x - 10;
-        var py = this.player.y - 8;
+        /*
+        //game.debug.text('speed: '+this.player.body.velocity.y, 10, 10);
+        game.debug.text('px: ' + this.player.x.toFixed(1), 10, 10);
+        game.debug.text('py: ' + this.player.y.toFixed(1), 10, 25);
+        
+        var px = this.player.x;
+        var py = this.player.y - this.player.height;
         var ledge = undefined;
         for (var i = 0; i < this.ledges.length; i++) {
+            if (i != 2) { this.ledges[i].alpha = 0.3; continue; }
+            
             ledge = this.ledges[i];
-            if (px < ledge.worldX-16 || px > ledge.worldX+16) { continue; }
-            if (py < ledge.worldY-8 || py > ledge.worldY) { continue; }
+            game.debug.text('posY: ' + ledge.worldY, 10, 40);
+            if (px < ledge.worldX-1 || px > ledge.worldX+16) { continue; }
+            if (py < ledge.worldY-8 || py > ledge.worldY-7) { continue; }
             
             var ledgeOnLeft = ledge.worldX < this.player.x;
             var ledgeOnRight = !ledgeOnLeft;
             
-            game.debug.text('grab: '+ledgeOnRight, 10, 40);
+            game.debug.text('grab: ' + ledgeOnRight, 10, 55);
+            // this.grabLedge();
             
             if (this.input.right.isDown && ledgeOnRight) {
                 this.player.body.enable = false;
                 this.player.body.velocity.y = 0;
+                this.grabLedge();
             } else if (this.input.left.isDown && ledgeOnLeft) {
-                this.player.body.enable = false;
-                this.player.body.velocity.y = 0;
-            }
-        }
-        
-        /*
-        var t = false;
-        var px = this.player.x - 8;
-        var py = this.player.y - 8;
-        if (px >= 9*16 && px < 11*16) {
-            if (py > 10*16 && py < 11*16) {
-                t = true
-            }
-        }
-        game.debug.text(py + " > " + (11*16), 10, 10);
-        
-        if (t) {
-            var ledgeOnLeft = 10 * 16 < this.player.x;
-            var ledgeOnRight = !ledgeOnLeft;
-
-            if (this.input.right.isDown) {
-                game.debug.text('grab:'+ledgeOnRight,10,40)
                 this.player.body.enable = false;
                 this.player.body.velocity.y = 0;
             }
@@ -224,11 +212,13 @@ TheGame.prototype = {
     },
     
     grabLedge: function () {
-        
+        // this.player.animations.play('ledge_grab', 12);
     },
     
     parallaxBackground: function () {
-        this.background.tilePosition.x = this.layer.x * -0.1;
-        this.middleground.tilePosition.x = this.layer.x * -0.5;
+        this.middleground1.tilePosition.x = this.layer.x * -0.1;
+        this.middleground2.tilePosition.x = this.layer.x * -0.2;
+        this.middleground3.tilePosition.x = this.layer.x * -0.3;
+        this.middleground4.tilePosition.x = this.layer.x * -0.4;
     }
 }
